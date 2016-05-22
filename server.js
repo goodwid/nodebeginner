ff8de9cdfff3be57bbe0f1a4e4aead13803a1d26
goodwid <goodwid@gmail.com>
2016-05-21 17:00:37 -0700

    modules.

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var http = require ('http');
 
-http.createServer(function(req, res) {
-  console.log('request received');
-  res.writeHead(200, {'Content-Type':'text/plain'});
-  res.write('Hello World');
-  res.end();
-}).listen(9000);
+function start() {
+  function onRequest(req, res) {
+    console.log('request received');
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.write('Hello World');
+    res.end();
+  }
+  http.createServer(onRequest).listen(9000);
+  console.log('server started');
+}
 
-console.log('server started');
+exports.start = start;
