92ae28883eaedb2a366e99cb40e7dcff05945346
goodwid <goodwid@gmail.com>
2016-05-21 17:53:26 -0700

    Handling POST

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,15 +3,21 @@ const exec = require('child_process').exec;
 function start(res) {
   console.log('handler "start" called');
 
-  let content = 'empty';
-  exec('ls -la', (err, stdout, stderr) => {
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.write(stdout);
-    res.end();
-  });
-
-  return content;
-
+  const body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="POST">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+  res.writeHead(200, {'Content-Type':'text/html'});
+  res.write(body);
+  res.end();
 }
 
 function upload(res) {
@@ -19,7 +25,6 @@ function upload(res) {
   res.writeHead(200, {'Content-Type':'text/plain'});
   res.write('Hello Upload');
   res.end();
-
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -3,14 +3,20 @@ const url = require ('url');
 
 function start(route, handle) {
   function onRequest(req, res) {
+    let body = '';
     const pathName = url.parse(req.url).pathname;
     console.log(`request for ${pathName} received.`);
 
-
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    route(handle, pathName, res);
-    
+    req.setEncoding('utf-8');
+    req.on('data', chunk => {
+      body += chunk;
+      console.log(`received POST data: \n${body}\n`);
+    });
+    req.on('end', () => {
+      route(handle, pathName, res);
+    });
   }
+
   http.createServer(onRequest).listen(9000);
   console.log('server started');
 }
